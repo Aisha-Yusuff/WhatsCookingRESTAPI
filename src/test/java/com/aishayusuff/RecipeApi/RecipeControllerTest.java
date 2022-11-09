85a8069acc3e20078965c8032c8c06d8f10ae66b
Aisha Yusuff <a.yusuff99@outlook.com>
2022-11-09 22:02:53 +0000

    Create controller unit test for post request. Ref #11

--- src/test/java/com/aishayusuff/RecipeApi/RecipeControllerTest.java
+++ src/test/java/com/aishayusuff/RecipeApi/RecipeControllerTest.java
@@ -19,9 +19,11 @@ import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.BDDAssertions.then;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 //Spring Test Class
 @ExtendWith(SpringExtension.class)
@@ -39,13 +41,14 @@ public class RecipeControllerTest {
 // JSON tester - to convert recipe list to json to compare against req body
     @Autowired
     private JacksonTester<List<Recipe>> json;
+    private JacksonTester<Recipe> jsonRequest;
 
     @Test
     void shouldReturnListOfAllRecipes() throws Exception {
 //        given
 //        build a recipe (without ingredients)
         Recipe porridgeRecipe = Recipe.builder()
-                .name("Quick and easy Porridge")
+                .name("Quick and Easy Porridge")
                 .instructions("""
                         1.Put your porridge oats in a saucepan.\s
                         2.Pour the milk into the saucepan.\s
@@ -76,6 +79,44 @@ public class RecipeControllerTest {
         then(response.getContentAsString()).isEqualTo(json.write(recipeList).getJson());
         verify(recipeService).getAllRecipes();
 
+    }
+
+    @Test
+    public void shouldCreateNewRecipe() throws Exception {
+//        given
+//        build a new recipe to add to recipe list
+        Recipe vegSoup = Recipe.builder().name("Veggie Soup for One")
+                .instructions(""" 
+                1.Pour the water in a medium-sized pot. 
+                2.Add your stock cubes and seasonings. 
+                3.Chop your veggies and place them in your pot. 
+                4.Leave your soup to cook for 20 minutes on a medium heat. 
+                5. If you like your veggies soft let them cook for another 10 minutes and then serve your soup.
+                """)
+                .build();
+
+        Ingredient water = new Ingredient("Water", "500ml", vegSoup.getId());
+        Ingredient carrot = new Ingredient("Carrot", "150g", vegSoup.getId());
+        Ingredient celery = new Ingredient("Celery", "150g", vegSoup.getId());
+        Ingredient potato = new Ingredient("Potato", "1", vegSoup.getId());
+        Ingredient stockCubes = new Ingredient("Stock Cubes", "2", vegSoup.getId());
+        Ingredient salt = new Ingredient("Salt", "1tbsp", vegSoup.getId());
+        Ingredient blackPepper = new Ingredient("Black Pepper", "1tbsp", vegSoup.getId());
+        Ingredient paprika = new Ingredient("Paprika", "1tbsp", vegSoup.getId());
+
+        Set<Ingredient> soupIngredientSet = new HashSet<>(Arrays.asList
+                (water, carrot, celery, potato, stockCubes, salt, blackPepper, paprika));
+        vegSoup.setIngredients(soupIngredientSet);
+        given(recipeService).addNewRecipe(any(Recipe.class)).willReturn(vegSoup);
+//        when - post request to add new recipe
+        MockHttpServletResponse response = mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(jsonRequest.write(vegSoup)
+                        .getJson())).andReturn().getResponse();
+
+//        then
+        then(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        then(response.getContentAsString()).isEqualTo(jsonRequest.write(vegSoup).getJson());
     }
 
 
