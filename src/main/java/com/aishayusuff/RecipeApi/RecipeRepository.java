ef7e7dbbb356d4ebf99a6e095cccd147ba977609
Aisha Yusuff <a.yusuff99@outlook.com>
2022-11-18 02:29:27 +0000

    Regenerate open api doc

--- src/main/java/com/aishayusuff/RecipeApi/RecipeRepository.java
+++ src/main/java/com/aishayusuff/RecipeApi/RecipeRepository.java
@@ -2,11 +2,9 @@ package com.aishayusuff.RecipeApi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
