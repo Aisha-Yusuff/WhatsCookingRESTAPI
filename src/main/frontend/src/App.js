392f7d136ca10f474a273e48cf064ceb989f7556
Aisha Yusuff <a.yusuff99@outlook.com>
2022-11-19 22:41:26 +0000

    Set up react router and create pages

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,8 +1,21 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { RecipeForm } from "./pages/RecipeForm";
+import { SearchResults } from "./pages/SearchResults";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/recipes" element={<Home />} />
+          <Route path="/results" element={<SearchResults />} />
+          <Route path="/recipeForm" element={<RecipeForm />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
