5a9de6260f4504ef0c6353c9658afc8d9e5bef2b
Aisha Yusuff <a.yusuff99@outlook.com>
2022-11-20 00:54:58 +0000

    Install tailwind and fetch recipes from java backend with axios

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { RecipeForm } from "./pages/RecipeForm";
-import { SearchResults } from "./pages/SearchResults";
+import { Recipe } from "./pages/Recipe";
 
 function App() {
   return (
@@ -10,8 +10,8 @@ function App() {
       <Router>
         <Routes>
           <Route path="/recipes" element={<Home />} />
-          <Route path="/results" element={<SearchResults />} />
           <Route path="/recipeForm" element={<RecipeForm />} />
+          <Route path="/recipeName" element={<Recipe />} />
         </Routes>
       </Router>
     </div>
--- src/main/frontend/src/pages/Home.js
+++ src/main/frontend/src/pages/Home.js
@@ -1,7 +1,50 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export const Home = () => {
-  return (
-    <div>
-      <p>Home page</p>
-    </div>
-  );
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchRecipes = () => {
+    axios.get("http://localhost:8080/recipe").then((res) => {
+      setRecipes(res.data);
+      console.log(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  return recipes.map((recipe, index) => {
+    return (
+      <div className="bg-blue-100">
+        {/* recipe */}
+        <div key={index}>
+          <h1>{recipe.name}</h1>
+
+          {/* ingredients  */}
+          <div>
+            {recipe.ingredients.map((ingredient) => (
+              <div>
+                <p>
+                  {ingredient.quantity} {ingredient.name}
+                </p>
+              </div>
+            ))}
+          </div>
+
+          {/* instructions */}
+          <div>
+            {recipe.instructions.map((instruction) => (
+              <div>
+                <p>
+                  {instruction.step_number}. {instruction.step_description}
+                </p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  });
 };
